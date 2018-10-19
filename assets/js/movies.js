d8d54d7565f8a5f31ee41c90805d9155e74c5c08
Timo <ttimoww@gmail.com>
2018-10-19 15:36:54 +0200

    Load in movie schedule fron JSON files & add movie trailers on popup

---
+++ assets/js/movies.js
@@ -0,0 +1,83 @@
+// VARIABLES
+var trailerContainer = document.querySelector(".trailer-container");
+var trailerIframe = document.querySelector("#trailer-iframe")
+
+// FILL STAGE TABLE
+function fillTable(stage){
+  fetch("../../assets/json/" + stage + ".json")
+  .then(res => res.json())
+  .then(function(data){
+
+    let tableBody = document.querySelector("#tbody-" + stage);
+
+    for(let day of data.list){
+      // Create tr with day and date
+      var tr = document.createElement('tr');
+      tableBody.appendChild(tr);
+        var th = document.createElement('th');
+        th.colSpan = "5";
+        th.classList.add("table-day");
+        tr.appendChild(th);
+          var p = document.createElement('p');
+          p.innerHTML = day.day + ' ' + day.date;
+          th.appendChild(p);
+
+      // Fill the table with movie info
+      for(let movie of day.movies){
+        var tr = document.createElement('tr');
+        tableBody.appendChild(tr);
+
+          // Movie Title
+          var title = document.createElement('th');
+          title.scope = "row";
+          title.classList.add("movie-title");
+          title.innerHTML = movie.title;
+          tr.appendChild(title);
+
+          // Movie Genre
+          var genre = document.createElement('td');
+          genre.innerHTML = movie.genre;
+          tr.appendChild(genre);
+
+          // Movie time
+          var time = document.createElement('td');
+          time.innerHTML = movie.time;
+          tr.appendChild(time);
+
+          // Movie age
+          var age = document.createElement('td');
+          age.innerHTML = movie.age;
+          tr.appendChild(age);
+
+          // Movie trailer
+          var trailer = document.createElement('td');
+          tr.appendChild(trailer);
+          var trailerimg = document.createElement('img');
+          trailerimg.src = "img/socials/youtube.png";
+          trailerimg.addEventListener("click", function(){initTrailer(movie.trailer)});
+          trailer.appendChild(trailerimg);
+
+      }
+    }
+  });
+}
+
+function initTrailer(url){
+  trailerContainer.style.display = "block";
+  trailerIframe.src = url;
+}
+
+// LISTENERS
+trailerContainer.addEventListener("click", function(){
+  trailerContainer.style.display = 'none';
+  trailerIframe.src = "";
+});
+
+// INITPAGE
+function initPage(){
+  fillTable("waterfront");
+  fillTable("forest");
+  fillTable("bridge");
+};
+
+initPage();
